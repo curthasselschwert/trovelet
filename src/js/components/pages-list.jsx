1c559f32724fe8dfa09b4426a0d9cc7e206bf400
Curt Hasselschwert <curthass@gmail.com>
2014-08-12 18:59:11 -0400

    Pagination for page search

--- src/js/components/pages-list.jsx
+++ src/js/components/pages-list.jsx
@@ -6,102 +6,77 @@ var AddPage    = require('./add-page');
 var emitter    = require('../emitter');
 var url        = require('url');
 var moment     = require('moment');
+var request    = require('browser-request');
 
 var PagesList = React.createClass({
 
   getInitialState: function() {
     return {
-      pageIds: [],
+      pages: [],
       error: {},
       limit: 5,
-      count: 0,
-      page: 1
+      api: '/pages/search?'
     };
   },
 
   componentDidMount: function() {
     emitter.on('page added', this.pageAdded);
-    emitter.on('page deleted', this.updateList);
+    emitter.on('page deleted', this.getPages.bind(this, this.state.url));
 
-    this.updateList();
-  },
-
-  getPages: function() {
-    var Page   = Parse.Object.extend('Page');
-    var query  = new Parse.Query(Page);
-    var user   = this.props.user;
-    var limit  = this.state.limit;
-    var offset = (this.state.page - 1) * this.state.limit;
-
-    query.equalTo('user', user);
-    query.descending('updatedAt');
-    query.limit(limit);
-    query.skip(offset);
-    query.select('id');
-    query.find()
-      .then(this.handleSuccess, this.handleError);
-  },
-
-  handleSuccess: function(pages) {
-    var ids = pages.map(function(page) { return page.id });
-
-    this.setState({ pageIds: ids });
-  },
-
-  handleError: function(error) {
-    this.setState({ error: error });
-  },
-
-  getCount: function() {
-    var Page  = Parse.Object.extend('Page');
-    var query = new Parse.Query(Page);
-    var user  = this.props.user;
-
-    query.equalTo('user', user);
-    query.count()
-      .then(this.setCount);
-  },
-
-  setCount: function(count) {
-    this.setState({ count: count });
-  },
-
-  setPage: function(page) {
-    this.setState({ page: page }, this.getPages);
-  },
-
-  updateList: function() {
     this.getPages();
-    this.getCount();
+  },
+
+  getPages: function(url) {
+    if (!url) {
+      var userId = 'userId=' + this.props.user.id;
+      var limit  = 'limit=' + this.state.limit;
+
+      url = this.state.api + [userId, limit].join('&')
+    }
+
+    var options = {
+      url: url,
+      json: true
+    }
+
+    this.setState({ url: url });
+    request(options, this.handleResponse);
+  },
+
+  handleResponse: function(error, response, body) {
+    if (error) {
+      return this.setState({ error: body });
+    }
+
+    console.log('Response', body);
+    this.setState(body);
   },
 
   pageAdded: function() {
-    this.setState({ page: 1 }, this.updateList);
+    this.getPages();
   },
 
   nextPage: function() {
-    var more   = this.state.page * this.state.limit < this.state.count;
-    var next   = this.state.page + 1;
-    var active = more ? 'active' : null;
+    var next   = this.state.next;
+    var active = next ? 'active' : null;
     var cname  = ['nav', 'next', active].join(' ');
-    var action = active ? this.setPage.bind(this, next) : function() {};
+    var action = active ? this.getPages.bind(this, this.state.next) : function() {};
 
     return <img src="/assets/images/nav-arrow.svg" className={ cname } onClick={ action } />;
   },
 
   prevPage: function() {
-    var less   = this.state.page > 1;
-    var prev   = this.state.page - 1;
-    var active = less ? 'active' : null;
+    var prev   = this.state.prev;
+    var active = prev ? 'active' : null;
     var cname  = ['nav', 'prev', active].join(' ');
-    var action = active ? this.setPage.bind(this, prev) : function() {};
+    var action = active ? this.getPages.bind(this, this.state.prev) : function() {};
 
     return <img src="/assets/images/nav-arrow.svg" className={ cname } onClick={ action } />;
   },
 
   pages: function() {
-    return this.state.pageIds.map(function(id) {
-      return <PageItem key={ id } id={ id } />;
+    return this.state.pages.map(function(page) {
+      return <PageItem key={ page.id } id={ page.id } />;
     });
   },
 
@@ -121,6 +96,7 @@ var PagesList = React.createClass({
       <div className="pages-list">
         <div className="page-tools">
           <div className="pagination">
+            Page { this.state.page } of { this.state.total_pages }
             { this.prevPage() }
             { this.nextPage() }
           </div>
